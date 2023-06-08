8101795bc2d549fd69860b1eac2538065396da9a
Ronak-Rathod-tech <rathodronak444@gmail.com>
2023-06-08 10:59:39 +0530

    new react project

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn
+          Learn React Form Tutorials
         </a>
       </header>
     </div>
